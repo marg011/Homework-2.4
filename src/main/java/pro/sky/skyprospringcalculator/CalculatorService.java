ea1fb6928921301ee4f4c596b45718f71c506ef7
margo <margo171190@mail.ru>
2023-06-08 16:01:00 +0400

    homework is done

---
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.skyprospringcalculator;
+
+public interface CalculatorService {
+    String hello();
+    String plus(String num1, String num2);
+    String minus(String num1, String num2);
+    String multiply(String num1, String num2);
+    String divide(String num1, String num2);
+}
